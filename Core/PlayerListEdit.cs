630a52160a8fec46aff93922348b096c2be6c42c
flye-name <73665110+flye-name@users.noreply.github.com>
2026-05-27 01:14:43 +0200

    player list pronouns

---
+++ Core/PlayerListEdit.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using MonoMod.Cil;
+using PronounsMod.Core.Players;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+using Terraria.IO;
+using Terraria.ModLoader;
+
+namespace PronounsMod.Core;
+
+public class PlayerListEdit : ModSystem
+{
+	public override void Load()
+	{
+		IL_UICharacterListItem.DrawSelf += IL_DrawSelf;
+	}
+
+	private void IL_DrawSelf(ILContext il)
+	{
+		ILCursor c = new ILCursor(il);
+
+		int textIndex = -1; // loc
+
+		c.GotoNext(MoveType.After, 
+			i => i.MatchLdfld<FileData>(nameof(UICharacterListItem._data.Name)),
+			i => i.MatchStloc(out textIndex)
+		);
+
+		c.EmitLdarg0();
+		c.EmitLdloca(textIndex);
+
+		c.EmitDelegate(AppendPronouns);
+	}
+
+	private static void AppendPronouns(UICharacterListItem self, ref string text)
+	{
+		Player player = self._data._player;
+		if (player.TryGetModPlayer<PlayerPronoun>(out var modPlayer))
+		{
+			text = text + " " + $"[c/b2aacc:({modPlayer.Pronoun.FullFormat})]";
+		}
+	}
+}
