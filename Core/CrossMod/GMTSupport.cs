0114eeda2ad1a2c399521bf99b4a68fb776e284d
flye-name <73665110+flye-name@users.noreply.github.com>
2026-06-05 19:17:23 +0200

    dave lang support

---
+++ Core/CrossMod/GMTSupport.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using GMT.Common;
+using GMT.Revamps.ForTheWorthy;
+using Hjson;
+using Newtonsoft.Json.Linq;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace PronounsMod.Core.CrossMod;
+
+[ExtendsFromMod("GMT")]
+public class GMTSupport : ModSystem
+{
+	private delegate void orig_LoadLanguage(bool force);
+	
+	public override void Load()
+	{
+		MethodInfo loadLanguage = typeof(FTWSystem).GetMethod("LoadLanguage", BindingFlags.Static | BindingFlags.Public);
+		
+		MonoModHooks.Add(loadLanguage, OnLoadLanguage);
+	}
+
+	private void OnLoadLanguage(orig_LoadLanguage orig, bool force)
+	{
+		bool shouldReplace = (Main.getGoodWorld || force) && !CommonSystem.dave;
+			
+		orig(force);
+		
+		if (shouldReplace)
+			DaveLangSupport.ReplaceDaveLangEntries();
+	}
+}
