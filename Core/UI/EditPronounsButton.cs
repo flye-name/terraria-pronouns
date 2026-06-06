ad07b9cb21820aa02825444e45bc43e3f6d36b5e
flye-name <73665110+flye-name@users.noreply.github.com>
2026-06-07 01:01:26 +0200

    edit button

--- Core/UI/EditPronounsButton.cs
+++ Core/UI/EditPronounsButton.cs
@@ -1,12 +1,16 @@
+using System.Linq;
 using Microsoft.Xna.Framework.Graphics;
 using MonoMod.Cil;
+using PronounsMod.Core.Players;
 using ReLogic.Content;
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ID;
+using Terraria.IO;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.ModLoader.UI;
 using Terraria.UI;
 
 namespace PronounsMod.Core.UI;
@@ -43,7 +47,7 @@ public class EditPronounsButton : ModSystem
 		UIImageButton uIImageButton = new UIImageButton(ButtonAsset);
 		uIImageButton.VAlign = 1f;
 		uIImageButton.Left.Set(num, 0f);
-		uIImageButton.OnLeftClick += ButtonClick;
+		uIImageButton.OnLeftClick += (evt, element) => SetUIState(self._data);
 		uIImageButton.OnMouseOver += (_, __) => self._buttonLabel.SetText(Language.GetTextValue("Mods.PronounsMod.UI.EditPronouns"));
 		uIImageButton.OnMouseOut += (_, __) => self._buttonLabel.SetText("");
 		self.Append(uIImageButton);
@@ -52,8 +56,55 @@ public class EditPronounsButton : ModSystem
 		uIImageButton.SetSnapPoint("EditPronouns", orderInList);
 	}
 
-	private void ButtonClick(UIMouseEvent evt, UIElement element)
+	private void SetUIState(PlayerFileData data)
 	{
-		SoundEngine.PlaySound(SoundID.MenuTick);
+		SoundEngine.PlaySound(SoundID.MenuOpen);
+
+		Player player = data._player;
+		Pronoun oldPronouns = player.GetModPlayer<PlayerPronoun>().Pronoun;
+		PronounMode oldMode = player.GetModPlayer<PlayerPronoun>().Mode;
+
+		UIText header = new(Language.GetTextValue("Mods.PronounsMod.UI.EditPronounsLarge", player.name), 1, true)
+		{
+			VAlign = .28f,
+			Width = StyleDimension.FromPercent(1)
+		};
+			
+		UICharacterCreationPronounsPanel pronounPanel = new(player, null)
+		{
+			Width = StyleDimension.FromPixels(550f),
+			Height = StyleDimension.FromPixels(380),
+			HAlign = .5f,
+			VAlign = .57f,
+		};
+		
+		UIButton<string> backButton = new(Language.GetTextValue("UI.Back"),0.7f, true)
+		{
+			Width = StyleDimension.FromPixels(260f),
+			Height = StyleDimension.FromPixels(50f),
+			Left = StyleDimension.FromPixels(-140f),
+			Top = StyleDimension.FromPixels(250f),
+			HAlign = .5f,
+			VAlign = .52f,
+		};
+		backButton.OnLeftClick += (evt, element) => UIEditPronounState.RevertPronouns(player, oldPronouns, oldMode); 
+		
+		UIButton<string> saveButton = new(Language.GetTextValue("UI.Save"),0.7f, true)
+		{
+			Width = StyleDimension.FromPixels(260f),
+			Height = StyleDimension.FromPixels(50f),
+			Left = StyleDimension.FromPixels(140f),
+			Top = StyleDimension.FromPixels(250f),
+			HAlign = .5f,
+			VAlign = .52f,
+		};
+		saveButton.OnLeftClick += (evt, element) => UIEditPronounState.SavePronouns(data);
+
+		UIState state = new UIEditPronounState(data, oldPronouns, oldMode);
+		state.Append(header);
+		state.Append(pronounPanel);
+		state.Append(backButton);
+		state.Append(saveButton);
+		Main.MenuUI.SetState(state);
 	}
 }
---
+++ Core/UI/UIEditPronounState.cs
@@ -0,0 +1,62 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using PronounsMod.Core.Players;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.UI;
+
+namespace PronounsMod.Core.UI;
+
+public class UIEditPronounState : UIState
+{
+	public PlayerFileData data;
+	public Pronoun oldPronoun;
+	public PronounMode oldMode;
+	public UIEditPronounState(PlayerFileData data, Pronoun oldPronoun, PronounMode oldMode)
+	{
+		this.data = data;
+		this.oldPronoun = oldPronoun;
+		this.oldMode = oldMode;
+		
+		Width = StyleDimension.FromPercent(1);
+		Height = StyleDimension.FromPercent(1);
+	}
+
+	public override void Update(GameTime gameTime)
+	{
+		base.Update(gameTime);
+
+		if (Main.keyState.IsKeyDown(Keys.Enter))
+		{
+			SavePronouns(data);
+		}
+
+		if (Main.keyState.IsKeyDown(Keys.Escape))
+		{
+			RevertPronouns(data._player, oldPronoun, oldMode);
+		}
+	}
+
+	public static void RevertPronouns(Player player, Pronoun oldPronouns, PronounMode oldMode)
+	{
+		SoundEngine.PlaySound(SoundID.MenuClose);
+		
+		player.GetModPlayer<PlayerPronoun>().Pronoun = oldPronouns;
+		player.GetModPlayer<PlayerPronoun>().Mode = oldMode;
+		
+		Main.MenuUI.SetState(Main._characterSelectMenu); 
+	}
+
+	public static void SavePronouns(PlayerFileData data)
+	{
+		SoundEngine.PlaySound(SoundID.MenuTick);
+		
+		Player.SavePlayer(data);
+		
+		SoundEngine.PlaySound(SoundID.ResearchComplete);
+		
+		Main.MenuUI.SetState(Main._characterSelectMenu); 
+	}
+}
